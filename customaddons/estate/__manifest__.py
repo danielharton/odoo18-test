c6fc1f42c5dd0836ae8fff79bcc4e3718b2a139b
danielharton <hartondaniel11@gmail.com>
2025-04-14 14:56:57 +0300

    Settings menu with property types

--- customaddons/estate/__manifest__.py
+++ customaddons/estate/__manifest__.py
@@ -4,8 +4,8 @@
     'data':[
         'security/ir.model.access.csv',
 
-        'views/estate_property_views.xml',
         'views/property_types_views.xml',
+        'views/estate_property_views.xml',
         'views/estate_menus.xml',
 
 
